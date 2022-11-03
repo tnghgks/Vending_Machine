e49d501061c48a89af099c11e647873c428ab1b4
HOSU HAN <tnghgks@naver.com>
2022-11-03 09:38:05 +0900

    🔥 파일 숨기기 ( gitignore 설정 )

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { drinkType, ownColaList, tempList } from "./fakeDB";
+
 const $ownDrinkList = document.querySelector("#ownDrinkList");
 const $vendingMenu = document.querySelector("#vendingMenu");
 const $shoppingList = document.querySelector("#shoppingList");
@@ -9,73 +11,9 @@ const $moneyInput = document.querySelector("#money_input");
 const $vendingItem = document.getElementsByClassName("cola_wrapper");
 const $MoneyReturnBtn = document.querySelector("#MoneyReturnBtn");
 const $totalPay = document.querySelector("#totalPay");
-
+console.log(drinkType);
 let totalMoney = 25000;
 let leftMoney = 1000;
-
-// 가짜 DB
-const drinkType = [
-  {
-    id: 1,
-    name: "Original_Cola",
-    imgPath: "img/Original_Cola.png",
-    alt: "빨간 오리지널 캔 콜라",
-    price: 1000,
-    count: 3,
-  },
-  {
-    id: 2,
-    name: "Violet_Cola",
-    imgPath: "img/Violet_Cola.png",
-    alt: "보라색 캔 콜라",
-    price: 1000,
-    count: 0,
-  },
-  {
-    id: 3,
-    name: "Yellow_Cola",
-    imgPath: "img/Yellow_Cola.png",
-    alt: "노란 캔 콜라",
-    price: 1000,
-    count: 3,
-  },
-  {
-    id: 4,
-    name: "Cool_Cola",
-    imgPath: "img/Cool_Cola.png",
-    alt: "하늘색 캔 콜라",
-    price: 1000,
-    count: 3,
-  },
-  {
-    id: 5,
-    name: "Green_Cola",
-    imgPath: "img/Green_Cola.png",
-    alt: "녹색 캔 콜라",
-    price: 1000,
-    count: 3,
-  },
-  {
-    id: 6,
-    name: "Orange_Cola",
-    imgPath: "img/Orange_Cola.png",
-    alt: "오렌지색 캔 콜라",
-    price: 1000,
-    count: 3,
-  },
-];
-
-let ownColaList = [
-  { name: "Original_Cola", count: 1 },
-  { name: "Green_Cola", count: 2 },
-  { name: "Orange_Cola", count: 1 },
-  { name: "Violet_Cola", count: 5 },
-];
-
-let tempList = [
-  { name: "Original_Cola", count: 1 },
-  { name: "Green_Cola", count: 2 },
-];
 
 // drinkType에서 매치되는 콜라 찾기
 function findDrinkObjs(obj) {
